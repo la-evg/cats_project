1bfb4adc00c1d0a25ad22bf19b7dc1591d735761
unknown <artem_larin@yoniti.ru>
2023-02-24 14:03:50 +0300

    Добавлен функционал добавление кота в избранные

--- api.js
+++ api.js
@@ -1,16 +1,33 @@
 class CatsApi {
-    constructor(apiName) {
-      this.url = `https://cats.petiteweb.dev/api/single/${apiName}`
-    }
-    
-    getCountCat() {
-        return fetch (`${this.url}/ids`)
-    }
-
-    getAllCats() {
-        return fetch(`${this.url}/show`)
-      }
+  constructor(apiName) {
+    this.url = `https://cats.petiteweb.dev/api/single/${apiName}`
   }
-  
-  const DB_NAME = 'la-evg';
-  const api = new CatsApi(DB_NAME);
+
+  // Запрос на получение количества айдишников в базе
+  getCountCat() {
+    return fetch(`${this.url}/ids`)
+  }
+  // Запрос на получение всех котов в базе
+  getAllCats() {
+    return fetch(`${this.url}/show`)
+  }
+
+  // Запрос на получение одного кота по id
+  getCurrentCat(id) {
+    return fetch(`${this.url}/show/${id}`)
+  }
+
+  // Запрос на изменение данных кота по id
+  changeCurrentCat(id, data) {
+    return fetch(`${this.url}/update/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  }
+}
+
+const DB_NAME = 'la-evg';
+const api = new CatsApi(DB_NAME);
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/// Плашка в шапке сайта под количество котов
+// Плашка в шапке сайта под количество котов
 const $countCats = document.querySelector('[data-count_cats]')
 const $wrapper = document.querySelector('[data-wrapper]')
 
 
-/// Функция для вывода количества айдишников в базе
+// Функция для вывода количества айдишников в базе
 const gettingCountCats = async () => {
     const res = await api.getCountCat();
     const data = await res.json();
@@ -11,10 +11,12 @@ const gettingCountCats = async () => {
 }
 gettingCountCats();
 
+// Генерация карточки с котом
 const generateCatCard = (cat) => {
     return (
         `<div class="col m-2 p-0" data-card_id=${cat.id}>
       <div class="card" style="width: 18rem;">
+      <i class="${cat.favorite ? "fa-solid" : "fa-regular"} fa-heart like" data-action="setLike"></i>
           <img class="object-fit-cover rounded" style="height: 250px;"
               src="${cat.image}"
               class="card-img-top" alt="Фото кота ${cat.name}">
@@ -36,6 +38,7 @@ const generateCatCard = (cat) => {
     )
 }
 
+// Рендер карточек
 const firstGettingCats = async () => {
     const res = await api.getAllCats();
     const data = await res.json();
@@ -46,3 +49,40 @@ const firstGettingCats = async () => {
 }
 firstGettingCats();
 
+// Слушатель кнопок
+$wrapper.addEventListener('click', async (event) => {
+    const $currentCard = event.target.closest('[data-card_id]');
+    const action = event.target.dataset.action;
+    const catId = $currentCard.dataset.card_id;
+    switch (action) {
+        // Изменение статуса favorite (like)
+        case 'setLike':
+            try {
+                // Выполняем проверку статуса favorite
+                const res = await api.getCurrentCat(catId)
+                const data = await res.json()
+                const iconElem = $currentCard.querySelector('.like')
+                if (data.favorite) {
+                    // Меняем статус и изменяем class иконки
+                    data.favorite = false;
+                    iconElem.classList.toggle("fa-solid")
+                    iconElem.classList.toggle("fa-regular")
+                }
+                else {
+                    // Меняем статус и изменяем class иконки
+                    data.favorite = true;
+                    iconElem.classList.toggle("fa-regular")
+                    iconElem.classList.toggle("fa-solid")
+                }
+                // Меняем статус и изменяем class иконки
+                api.changeCurrentCat(catId, data)
+
+            } catch (error) {
+                console.log(error)
+            }
+            break;
+
+        default:
+            break;
+    }
+})
